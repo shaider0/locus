5412ed2791b07bf309bf378f3c7e3c6c38dc9508
SHaider0 <Samnoon.Haider@gmail.com>
2019-04-25 13:34:38 -0400

    Generate scaffold for goal resource
    -Destroyed scaffold for example resource
    -Rolled back DB migration for example resource
    -Checked goal resource migration for accuracy
    -Ran DB migration for goal resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 2019_04_25_172743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "examples", force: :cascade do |t|
-    t.text "text", null: false
-    t.bigint "user_id", null: false
+  create_table "goals", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_examples_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,5 +32,4 @@ ActiveRecord::Schema.define(version: 2) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
-  add_foreign_key "examples", "users"
 end
