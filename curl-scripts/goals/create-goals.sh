d37006c8c04aed3b8127d13fd13d05a38e68d7dc
SHaider0 <Samnoon.Haider@gmail.com>
2019-04-25 14:35:24 -0400

    write and test curl script to create a goal
    -also tested curl script using index action to confirm that
    I can view all created goals

--- curl-scripts/goals/create-goals.sh
+++ curl-scripts/goals/create-goals.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-curl "http://localhost:4741/examples" \
+curl "http://localhost:4741/goals" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+    "goal": {
+      "title": "'"${TITLE}"'",
+      "description": "'"${DESCRIPTION}"'",
+      "user_id": "'"${USER}"'"
     }
   }'
 
